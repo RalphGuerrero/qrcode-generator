34cbfedd39c123815104872c700a832f05edbe52
RalphGuerrero <ralphsilvaguerrero@gmail.com>
2021-10-22 07:09:12 -0300

    Adding some colors

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
 
   return (
     <div className="App">
+      <h1>QR Code Generator</h1>
+      <p>Please type down your url to create your code</p>
       <input
         onChange={(element) => setText(element.target.value)}
         value={text}
       ></input>
       <QRCodeCanvas text={text}></QRCodeCanvas>
+      <h6>developed by Ralph Guerrero</h6>
     </div>
   );
 }
